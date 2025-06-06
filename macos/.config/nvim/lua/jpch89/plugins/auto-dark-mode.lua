fa139426125d415ebf54e400408ed92c9680351d
jpch89 <jpch89@outlook.com>
2025-06-06 21:11:28 +0800

    make auto-dark-mode compatible for lualine

--- macos/.config/nvim/lua/jpch89/plugins/auto-dark-mode.lua
+++ macos/.config/nvim/lua/jpch89/plugins/auto-dark-mode.lua
@@ -4,10 +4,60 @@ return {
 		set_dark_mode = function()
 			vim.api.nvim_set_option_value("background", "dark", {})
 			vim.cmd("colorscheme onedark_vivid")
+
+			-- to configure lazy pending updates count
+			local lualine = require("lualine")
+			local lazy_status = require("lazy.status")
+			local onedark_vivid = require("lualine.themes.onedark_vivid")
+			lualine.setup(
+				-- configure lualine with modified theme
+				{
+					options = {
+						theme = onedark_vivid,
+					},
+					sections = {
+						lualine_x = {
+							{
+								lazy_status.updates,
+								cond = lazy_status.has_updates,
+								color = { fg = "#ff9e64" },
+							},
+							{ "encoding" },
+							{ "fileformat" },
+							{ "filetype" },
+						},
+					},
+				}
+			)
 		end,
 		set_light_mode = function()
 			vim.api.nvim_set_option_value("background", "light", {})
 			vim.cmd("colorscheme onelight")
+
+			-- to configure lazy pending updates count
+			local lualine = require("lualine")
+			local lazy_status = require("lazy.status")
+			local onelight = require("lualine.themes.onelight")
+			lualine.setup(
+				-- configure lualine with modified theme
+				{
+					options = {
+						theme = onelight,
+					},
+					sections = {
+						lualine_x = {
+							{
+								lazy_status.updates,
+								cond = lazy_status.has_updates,
+								color = { fg = "#ff9e64" },
+							},
+							{ "encoding" },
+							{ "fileformat" },
+							{ "filetype" },
+						},
+					},
+				}
+			)
 		end,
 		-- 10s to update
 		update_interval = 10000,
--- macos/.config/nvim/lua/jpch89/plugins/lualine.lua
+++ macos/.config/nvim/lua/jpch89/plugins/lualine.lua
@@ -1,28 +1,28 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-    local onedark = require("lualine.themes.onedark")
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		local onedark_vivid = require("lualine.themes.onedark_vivid")
 
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        theme = onedark,
-      },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				theme = onedark_vivid,
+			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
