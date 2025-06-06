cd6556cb3ed9572061273bc82b87a1d9b376be5a
jpch89 <jpch89@outlook.com>
2025-06-06 16:28:07 +0800

    modify .wezterm.lua, automatic switch b/w light/dark mode

--- macos/.wezterm.lua
+++ macos/.wezterm.lua
@@ -12,5 +12,26 @@ config.enable_tab_bar = false
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 
+-- color scheme
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "OneHalfDark"
+	else
+		return "OneHalfLight"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
 -- and finally, return the configuration to wezterm
 return config
