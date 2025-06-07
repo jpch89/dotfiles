33f569447516896a66984cc18c8958fa0dbc2b82
jpch89 <jpch89@outlook.com>
2025-06-07 11:03:24 +0800

    remove transparency for WezTerm

--- macos/.wezterm.lua
+++ macos/.wezterm.lua
@@ -9,8 +9,8 @@ config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16
 config.enable_tab_bar = false
 -- config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 20
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 20
 
 -- color scheme
 
