3778ec3f99208a2e0d1567250fa1589250c1209c
Patrick Ji <jpch89@outlook.com>
2025-07-02 21:42:26 +0800

    add configurations for PowerShell

---
+++ win/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+function Get-GitStatus { & git status -sb $args } 
+New-Alias -Name gst -Value Get-GitStatus -Force -Option AllScope 
+function Get-GitAddAll { & git add --all $args } 
+New-Alias -Name gaa -Value Get-GitAddAll -Force -Option AllScope 
+function Get-GitCommit { & git commit -m $args } 
+New-Alias -Name gcm -Value Get-GitCommit -Force -Option AllScope 
+function Get-GitPush { & git push --follow-tags $args } 
+New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope 
+function Get-GitPullRebase { & git pull --rebase $args } 
+New-Alias -Name gpr -Value Get-GitPullRebase -Force -Option AllScope 
+function Get-GitCheckout { & git checkout $args } 
+New-Alias -Name gco -Value Get-GitCheckout -Force -Option AllScope 
+function Get-GitCheckoutBranch { & git checkout -b $args } 
+New-Alias -Name gcb -Value Get-GitCheckoutBranch -Force -Option AllScope 
---
+++ win/powershell/powershell.md
@@ -0,0 +1,17 @@
+## PowerShell 配置文件说明
+
+**配置文件路径**
+
+配置文件路径位于 `$PROFILE`，我这里是：
+
+`C:\Users\Patrick Ji\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1`
+
+**命令别名**
+
+- `gst`：`git status`
+- `gaa`：`git add -A`
+- `gcm`：`git commit -m`
+- `gps`：`git push`
+- `gpr`：`git pull --rebase`
+- `gco`：`git checkout`
+- `gcb`：`git checkout -b`
