6232724b038709f7d22ee47b3ab9a184ca6eb1f4
Patrick Ji <jpch89@outlook.com>
2025-07-03 10:43:09 +0800

    update settings.zip, powershell configs

--- win/powershell/Microsoft.PowerShell_profile.ps1
+++ win/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,29 @@
+oh-my-posh init pwsh --config ~/powerlevel10k_rainbow.omp.json | Invoke-Expression
+
 # Git aliases
 function Get-GitStatus { & git status -sb $args }
-New-Alias -Name gst -Value Get-GitStatus -Force -Option AllScope
+New-Alias -Name gst -Value Get-GitStatus
 
-function Get-GitAddAll{ & git add --all $args }
-New-Alias -Name gaa -Value Get-GitAddAll -Force -Option AllScope
+function Get-GitAddAll { & git add --all $args }
+New-Alias -Name gaa -Value Get-GitAddAll
 
+Remove-Item -Path Alias:\gcm -Force -ErrorAction SilentlyContinue
 function Get-GitCommit { & git commit -m $args }
-New-Alias -Name gcm -Value Get-GitCommit -Force -Option AllScope
+New-Alias -Name gcm -Value Get-GitCommit
 
+Remove-Item -Path Alias:\gps -Force -ErrorAction SilentlyContinue
 function Get-GitPush { & git push --follow-tags $args }
-New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+New-Alias -Name gps -Value Get-GitPush
 
 function Get-GitPullRebase { & git pull --rebase $args }
-New-Alias -Name gpr -Value Get-GitPullRebase -Force -Option AllScope
+New-Alias -Name gpr -Value Get-GitPullRebase
 
-function Get-GitCheckout{ & git checkout $args }
-New-Alias -Name gco -Value Get-GitCheckout -Force -Option AllScope
+function Get-GitCheckout { & git checkout $args }
+New-Alias -Name gco -Value Get-GitCheckout
 
+Remove-Item -Path Alias:\gcb -Force -ErrorAction SilentlyContinue
 function Get-GitCheckoutBranch { & git checkout -b $args }
-New-Alias -Name gcb -Value Get-GitCheckoutBranch -Force -Option AllScope
+New-Alias -Name gcb -Value Get-GitCheckoutBranch
 
-function Get-GitLog{ & git log $args }
-New-Alias -Name glg -Value Get-GitLog -Force -Option AllScope
+function Get-GitLog { & git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit $args }
+New-Alias -Name glg -Value Get-GitLog
--- win/powershell/powershell.md
+++ win/powershell/powershell.md
@@ -8,12 +8,37 @@
 
 **命令别名**
 
-- `gst`：`git status`
+- `gst`：`git status -sb`
 - `gaa`：`git add -A`
 - `gcm`：`git commit -m`
 - `gps`：`git push`
 - `gpr`：`git pull --rebase`
 - `gco`：`git checkout`
 - `gcb`：`git checkout -b`
+- `glg`：`git log`
 
 **oh-my-posh**
+
+- 需要使用 `oh-my-posh font install meslo` 安装字体
+- 修改 `Windows Terminal` 的 `PowerShell` 配置文件 `settings.json`
+
+```json
+{
+    "profiles":
+    {
+        "defaults":
+        {
+            "font":
+            {
+                "face": "MesloLGM Nerd Font Mono"
+            }
+        }
+    }
+}
+```
+
+- 主题文件 `powerlevel10k_rainbow.omp.json` 放在用户家目录下才能生效，主题相关配置已经加入到 `Microsoft.PowerShell_profile.ps1`。
+
+```powershell
+oh-my-posh init pwsh --config ~/powerlevel10k_rainbow.omp.json | Invoke-Expression
+```
